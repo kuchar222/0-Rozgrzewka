1beec4b5ff5a2c51bb84e3d148e9c7f50aed72dd
Marcin Kucharski <kucharskimarcin.mk@gmail.com>
2022-02-27 18:12:21 +0100

    poprawki pylint

--- lotek.py
+++ lotek.py
@@ -1,8 +1,10 @@
 '''Ile muszę wydać żeby wygrać w lotka?'''
 
-import random, sys, colorama
+import random
+import sys
 from datetime import datetime
-from colorama import Fore, Back, Style
+import colorama
+from colorama import Fore
 
 colorama.init(autoreset=True)
 
@@ -19,17 +21,17 @@ def pobierz_liczby():
     Returns:
         set: podane przez użytkownika liczby
     """
-    moje_liczby = set()
+    liczby_gracza = set()
     i = 1
     while i < 7:
         liczba = int(input(f'Podaj {i} z 6 liczb zawierających się w przedzial od 1 do 49: '))
         if liczba in list(range(1, 50)):
-            moje_liczby.add(liczba)
+            liczby_gracza.add(liczba)
             i += 1
         else:
             print('Podana liczba nie mieści się w zakresie 1 - 49, spróbuj jeszcze raz')
 
-    return moje_liczby
+    return liczby_gracza
 
 def pobierz_wiek():
     """przyjmuje wiek od użytkownika
@@ -37,16 +39,14 @@ def pobierz_wiek():
     Returns:
         int: wiek użytkownika w latach
     """
-  
-    wiek = int(input('Podaj wiek, a odpowiem Ci, czy dożyjesz wygranej szóstki w lotka :) '))
+    wiek_gracza = int(input('Podaj wiek, a odpowiem Ci, czy dożyjesz wygranej szóstki w lotka :) '))
     if wiek < 18:
         print("Jesteś za młody/a żeby grać w lotto")
         sys.exit()
     if wiek > 60:
         print('W tym wieku szkoda tracić pieniędzy na lotka, kup wnukom po lodzie')
         sys.exit()
-        
-    return wiek
+    return wiek_gracza
 
 def losuj_liczbe(zbior_liczb):
     """losuje (wyciąga) liczbę ze zbioru liczb,
@@ -62,7 +62,7 @@ def losuj_liczbe(zbior_liczb):
     zbior_liczb.remove(wylosowana_liczba)
     return wylosowana_liczba, zbior_liczb
 
-def dodaj_liczbe_do_wylosowanych(wylosowana_liczba, wylosowane_liczby):
+def dodaj_liczbe_do_wylosowanych(wylosowana_liczba, liczby_wylosowane):
     """dodaje wylosowaną liczbę do zbioru liczb wylosowanych
 
     Args:
@@ -71,10 +71,10 @@ def dodaj_liczbe_do_wylosowanych(wylosowana_liczba, wylosowane_liczby):
     Returns:
         set: zbiór liczb wylosowanych po dodaniu kolejnej liczby
     """
-    wylosowane_liczby.add(wylosowana_liczba)
-    return wylosowane_liczby
+    liczby_wylosowane.add(wylosowana_liczba)
+    return liczby_wylosowane
 
-def wylosuj_liczby(liczby_w_maszynie, wylosowane_liczby, ilosc_losowanych_liczb=6):
+def wylosuj_liczby(liczby_w_maszynie_losujacej, wylosowane_liczby, ilosc_losowanych_liczb=6):
     """losuje zadaną ilość liczb tak jak w TV
 
     Args:
@@ -82,10 +82,9 @@ def wylosuj_liczby(liczby_w_maszynie, wylosowane_liczby, ilosc_losowanych_liczb=
     Returns:
         set: zbiór wylosowanych liczb
     """
-        
     i = 0
     while i < ilosc_losowanych_liczb:
-        wylosowana_liczba, liczby_w_maszynie = losuj_liczbe(liczby_w_maszynie)
+        wylosowana_liczba, liczby_w_maszynie_losujacej = losuj_liczbe(liczby_w_maszynie_losujacej)
         wylosowane_liczby = dodaj_liczbe_do_wylosowanych(wylosowana_liczba, wylosowane_liczby)
         i += 1
 
@@ -127,21 +126,17 @@ def oblicz_wiek(wiek, liczba_losowan):
     wiek_zwyciezcy = int(wiek + liczba_losowan/156)
     return wiek_zwyciezcy
 
-def oblicz_wygrana(trafiono_5, trafiono_4, trafiono_3):
+def oblicz_wygrana(TRAFIONO_5, TRAFIONO_4, TRAFIONO_3):
     """oblicza całkowitą wygraną za wszystkie trafienia do momentu trafnienia szóstki
 
     Args:
-        trafiono_5 (int): liczba trafionych piątek
-        trafiono_4 (int): liczba trafionych czwórek
-        trafiono_3 (int): liczba trafionych trójek
-        WYGRANA_SZ (int): wartość wygranej za szóstkę
-        WYGRANA_PI (int): wartość wygranej za piątkę
-        WYGRANA_CZW (int): wartość wygranej za czwórkę
-        WYGRANA_TR int): wartość wygranej za trójkę
+        TRAFIONO_5 (int): liczba trafionych piątek
+        TRAFIONO_4 (int): liczba trafionych czwórek
+        TRAFIONO_3 (int): liczba trafionych trójek
     Returns:
         int: całkowita wartość wygranej
     """
-    wygrana = WYGRANA_SZOSTKA + WYGRANA_PIATKA*trafiono_5 + WYGRANA_CZWORKA*trafiono_4 + WYGRANA_TROJKA*trafiono_3
+    wygrana = WYGRANA_SZOSTKA + WYGRANA_PIATKA*TRAFIONO_5 + WYGRANA_CZWORKA*TRAFIONO_4 + WYGRANA_TROJKA*TRAFIONO_3
     return wygrana
 
 def podaj_czas_liczenia(start, koniec):
@@ -155,7 +150,7 @@ def podaj_czas_liczenia(start, koniec):
     """
     return str(koniec - start)
 
-def podaj_podsumowanie(liczba_prob, trafiono_5, trafiono_4, trafiono_3, czas):
+def podaj_podsumowanie(LICZBA_PROB, TRAFIONO_5, TRAFIONO_4, TRAFIONO_3, czas):
     """podaje podsumowanie obliczeń:
     liczbę losowań
     liczbę trafień: piątek, czwórek, trójek
@@ -165,39 +160,39 @@ def podaj_podsumowanie(liczba_prob, trafiono_5, trafiono_4, trafiono_3, czas):
     wiek w chwili wygranej
 
     Args:
-        liczba_prob (int): całkowita liczba losowań
-        trafiono_5 (int): liczba trafionych piątek
-        trafiono_4 (int): liczba trafionych czwórek
-        trafiono_3 (int): liczba trafionych trójek
+        LICZBA_PROB (int): całkowita liczba losowań
+        TRAFIONO_5 (int): liczba trafionych piątek
+        TRAFIONO_4 (int): liczba trafionych czwórek
+        TRAFIONO_3 (int): liczba trafionych trójek
         czas (str): całkowity czas obliczania
     """
     print('---'*10)
-    print(f'{Fore.YELLOW}BRAWO udało się trafić szóstkę po {liczba_prob:,} losowaniach')
+    print(f'{Fore.YELLOW}BRAWO udało się trafić szóstkę po {LICZBA_PROB:,} losowaniach')
     print('Ponadto trafiłeś jeszcze:')
-    print(f'piątek:  {trafiono_5:,}')
-    print(f'czwórek: {trafiono_4:,}')
-    print(f'trójek:  {trafiono_3:,}')
+    print(f'piątek:  {TRAFIONO_5:,}')
+    print(f'czwórek: {TRAFIONO_4:,}')
+    print(f'trójek:  {TRAFIONO_3:,}')
     print('---'*10)
-    wygrana = oblicz_wygrana(trafiono_5, trafiono_4, trafiono_3)
-    wydano = oblicz_wartosc_losow(liczba_prob)
+    wygrana = oblicz_wygrana(TRAFIONO_5, TRAFIONO_4, TRAFIONO_3)
+    wydano = oblicz_wartosc_losow(LICZBA_PROB)
     print(f'{Fore.BLUE}Łącznie wygrałeś {wygrana:,}zł, a wydałeś na losy {wydano:,}zł')
-    print(f'Twój komputer wykonał te obliczenia w {czas}, a Ty miałbyś {oblicz_wiek(wiek, liczba_prob):,} lat w dniu wygranej')
+    print(f'Twój komputer wykonał te obliczenia w {czas}, a Ty miałbyś {oblicz_wiek(wiek, LICZBA_PROB):,} lat w dniu wygranej')
     print('Chyba trzeba brać się do kodowania, a nie czekać na wygraną :)')
     print()
 
 if __name__ == "__main__":
     wiek = pobierz_wiek()
-    moje_liczby = pobierz_liczby()   
+    moje_liczby = pobierz_liczby()
     start = datetime.now()
 
-    liczba_prob = 0
-    trafiono_5 = 0
-    trafiono_4 = 0
-    trafiono_3 = 0
+    LICZBA_PROB = 0
+    TRAFIONO_5 = 0
+    TRAFIONO_4 = 0
+    TRAFIONO_3 = 0
 
     while True:
-        liczba_prob += 1
-        print(f'Losowanie nr {liczba_prob:,}', end='\r')
+        LICZBA_PROB += 1
+        print(f'Losowanie nr {LICZBA_PROB:,}', end='\r')
 
         # losowanie liczb
         liczby_w_maszynie = list(range(1,50))
@@ -207,18 +202,18 @@ if __name__ == "__main__":
         # sprawdzanie liczby trafień
         trafienia = sprawdz_liczbe_trafien(wylosowane_liczby, moje_liczby)
         if trafienia == 6:
-            czas = podaj_czas_liczenia(start, datetime.now())
-            podaj_podsumowanie(liczba_prob, trafiono_5, trafiono_4, trafiono_3, czas)
+            CZAS = podaj_czas_liczenia(start, datetime.now())
+            podaj_podsumowanie(LICZBA_PROB, TRAFIONO_5, TRAFIONO_4, TRAFIONO_3, CZAS)
             break
-        elif trafienia == 5:
-            if trafiono_5 == 0:
-                print(f'{Fore.GREEN}Pierwsza trafiona piątka po {liczba_prob:,} losowaniach')
-            trafiono_5 += 1
+        if trafienia == 5:
+            if TRAFIONO_5 == 0:
+                print(f'{Fore.GREEN}Pierwsza trafiona piątka po {LICZBA_PROB:,} losowaniach')
+            TRAFIONO_5 += 1
         elif trafienia == 4:
-            if trafiono_4 == 0:
-                print(f'{Fore.GREEN}Pierwsza trafiona czwórka po {liczba_prob:,} losowaniach')
-            trafiono_4 += 1
+            if TRAFIONO_4 == 0:
+                print(f'{Fore.GREEN}Pierwsza trafiona czwórka po {LICZBA_PROB:,} losowaniach')
+            TRAFIONO_4 += 1
         elif trafienia == 3:
-            if trafiono_3 == 0:
-                print(f'{Fore.GREEN}Pierwsza trafiona trójka po {liczba_prob:,} losowaniach')
-            trafiono_3 += 1
+            if TRAFIONO_3 == 0:
+                print(f'{Fore.GREEN}Pierwsza trafiona trójka po {LICZBA_PROB:,} losowaniach')
+            TRAFIONO_3 += 1
